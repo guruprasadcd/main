d12ffd3abc5f55702d90267ba0f73bb73527feb6
gurup <gurup@Guru_Prasad>
2024-06-28 10:27:21 +0530

    demo

---
+++ src/main/java/demo/Chrome.java
@@ -0,0 +1,23 @@
+package demo;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// basics primitive and non-primitive data type
+		
+		float Version=91.5f;
+		String Developer="Chrome";
+		boolean Beta=false;
+		long Year=2008;
+		char cutkey='C';
+		
+		System.out.println(Version);
+		System.out.println(Developer);
+		System.out.println(Beta);
+		System.out.println(Year);
+		System.out.println(cutkey);
+		
+
+	}
+
+}
---
+++ src/main/java/demo/FindOddNumbers.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// WAP to print odd numbers from 1-10
+		
+		for(int i=1;i<=10;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
