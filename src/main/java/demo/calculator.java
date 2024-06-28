b3aaa3044ddaeae16c13d5e28db6a4db91b31c3f
gurup <gurup@Guru_Prasad>
2024-06-28 10:21:30 +0530

    practice

--- src/main/java/demo/calculator.java
+++ src/main/java/demo/calculator.java
@@ -5,10 +5,10 @@ public class calculator {
 	public static void main(String[] args) {
 		
 		//WAP to print numbers from 1-10
-//		for(int i = 1;i<=10;i++)
-//		{
-//			System.out.println(i);
-//		}
+	for(int i = 1;i<=10;i++)
+		{
+			System.out.println(i);
+	}
 		
 		//WAT to print numbers from 10-1
 		for(int j=10;j>=1;j--)
