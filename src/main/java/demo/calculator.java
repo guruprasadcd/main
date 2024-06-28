489b250c1dac5f62d729ab55e738d61bbb31af9a
gurup <gurup@Guru_Prasad>
2024-06-28 09:37:38 +0530

    first commit

---
+++ src/main/java/demo/calculator.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class calculator {
+
+	public static void main(String[] args) {
+		
+		//WAP to print numbers from 1-10
+//		for(int i = 1;i<=10;i++)
+//		{
+//			System.out.println(i);
+//		}
+		
+		//WAT to print numbers from 10-1
+		for(int j=10;j>=1;j--)
+		{
+			System.out.println(j);
+		}
+
+	}
+
+}
